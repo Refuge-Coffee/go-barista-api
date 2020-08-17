8f29e6ca52f87f8414e83562785c621afa491553
Taylor Jones <taylor.jones@salesforce.com>
2020-08-17 16:26:50 -0400

    template loading

--- barista.go
+++ barista.go
@@ -13,6 +13,11 @@ type Order struct {
 	Details []byte
 }
 
+func (o *Order) save() error {
+	filename := o.Name + ".txt"
+	return ioutil.WriteFile(filename, o.Details, 0600)
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hi there, I love %s", r.URL.Path[1:])
 }
@@ -20,6 +25,8 @@ func handler(w http.ResponseWriter, r *http.Request) {
 func main() {
 	http.HandleFunc("/", handler)
 	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/edit/", editHandler)
+  http.HandleFunc("/save/", saveHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
@@ -30,6 +37,28 @@ func loadOrder(name string) (*Order, error) {
 			return nil, err
 	}
 	return &Order{Name: name, Details: details}, nil
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	name := r.URL.Path[len("/edit/"):]
+	p, err := loadOrder(name)
+	if err != nil {
+			p = &Order{Name: name}
+	}
+	fmt.Fprintf(w, "<h1>Editing %s</h1>"+
+			"<form action=\"/save/%s\" method=\"POST\">"+
+			"<textarea name=\"details\">%s</textarea><br>"+
+			"<input type=\"submit\" value=\"Save\">"+
+			"</form>",
+			p.Name, p.Name, p.Details)
+}
+
+func saveHandler(w http.ResponseWriter, r *http.Request) {
+	name := r.URL.Path[len("/save/"):]
+	details := r.FormValue("details")
+	p := &Order{Name: name, Details: []byte(details)}
+	p.save()
+	http.Redirect(w, r, "/view/"+name, http.StatusFound)
 }
 
 func viewHandler(w http.ResponseWriter, r *http.Request) {
